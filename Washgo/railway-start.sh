c20e4ab6d9864618e1c094b16fbccfd2bcd61752
lequanghieu2504 <lequanghieu2504@gmail.com>
2025-09-20 20:37:45 +0700

    continue fixing

--- Washgo/railway-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Build and run script for Railway
-
-echo "Building with Maven..."
-./mvnw clean package -DskipTests
-
-echo "Starting application..."
-java -jar target/washgo-0.0.1-SNAPSHOT.jar
